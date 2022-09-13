12f3714ee629f89d971a0b24a90f7943106cdfce
ivorl <ivorl@10.0.0.57>
2022-09-13 15:26:52 -0600

    Test commit Commit to public repository

---
+++ SpendingSpree/src/spendingspree/SpendingSpree.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spendingspree;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ivorl
+ */
+public class SpendingSpree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double wallet = 200;
+        String thoughts = "";
+        
+        System.out.println("Honey are you going to the mall?((enter yes/no ): ");
+        
+        String userChoice = input.nextLine();
+        
+        if (userChoice.toLowerCase() == "yes"){
+            wallet = significantOtherGoesToTown(wallet);
+        }else {thoughts = "fewf";}
+       
+       // boolean spendingSpree = false;
+                
+                
+    }
+    
+    public static Double significantOtherGoesToTown(double wallet){
+        
+        while (wallet >=0){
+            System.out.println("ChaChing $$$");
+            wallet -= 50;
+            
+        }
+        return wallet;
+        
+    }
+    
+    }
+    
+
